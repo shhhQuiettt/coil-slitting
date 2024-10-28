15d9e208d9ab98df27146c88fa1f675cbf1fe0c7
shhhQuiettt <krzysztof.skro@gmail.com>
2024-10-28 14:40:09 +0100

    Set up slicing tree with library implementation

--- nsga2.py
+++ nsga2.py
@@ -1,4 +1,10 @@
 from dataclasses import dataclass
+from slicing_tree import (
+    Slit as SlicingTree,
+    generate_random_slitting_tree,
+    swap_random_subtree,
+    average_rectangle_size,
+)
 import numpy as np
 from copy import deepcopy
 import random
@@ -11,128 +17,10 @@ from pymoo.core.mutation import Mutation
 from pymoo.problems.multi import ElementwiseProblem
 from pymoo.optimize import minimize
 from pymoo.core.duplicate import NoDuplicateElimination
+from anytree import NodeMixin
 
 POPULATION_SIZE = 10
 MAX_NUMBER_OF_SLITS = 10
-
-
-@dataclass
-class Slit:
-    horizontal: bool
-    offset: float  # percentage of the width/height
-    left_slit: Optional["Slit"]
-    right_slit: Optional["Slit"]
-
-    def is_leaf(self) -> bool:
-        return self.left_slit is None and self.right_slit is None
-
-
-class SlicingTree:
-    def __init__(self, number_of_slits: int = 0, root: Optional[Slit] = None):
-        self.number_of_slits = number_of_slits
-        self.root: Optional[Slit] = root
-
-    def average_slit_size(self) -> float:
-        return 0
-
-    def average_slit_quality(self) -> float:
-        return 0
-
-    def complexity(self) -> float:
-        return 0
-
-    def __repr__(self):
-        return f"SlicingTree(number_of_slits={self.number_of_slits}, root={self.root is not None})"
-
-    @classmethod
-    def random(cls, max_number_of_slits: int) -> "SlicingTree":
-        no_of_slits = np.random.randint(1, max_number_of_slits)
-        root = generate_random_slitting_tree_root(no_of_slits)
-
-        return cls(no_of_slits, root)
-
-    def get_random_node(self) -> Optional[Slit]:
-        current = self.root
-        assert current is not None
-
-        while True:
-            if random.choice([True, False]):
-                current = current.left_slit
-            else:
-                current = current.right_slit
-
-            if random.random() < 0.2 or current is None:
-                return current
-
-    def get_random_leaf(self) -> Optional[Slit]:
-        current = self.root
-        while current is not None:
-            if current.left_slit is None and current.right_slit is None:
-                return current
-            if current.left_slit is None:
-                current = current.right_slit
-                continue
-
-            elif current.right_slit is None:
-                current = current.left_slit
-                continue
-
-            if random.choice([True, False]):
-                current = current.left_slit
-            else:
-                current = current.right_slit
-
-    def add_random_slit(self):
-        leaf = self.get_random_leaf()
-        if leaf is None:
-            return
-        if random.choice([True, False]):
-            leaf.left_slit = Slit(
-                horizontal=random.choice([True, False]),
-                offset=random.random(),
-                left_slit=None,
-                right_slit=None,
-            )
-        else:
-            leaf.right_slit = Slit(
-                horizontal=random.choice([True, False]),
-                offset=random.random(),
-                left_slit=None,
-                right_slit=None,
-            )
-
-    def remove_random_leaf(self):
-        current = self.root
-        assert current is not None
-        assert not current.is_leaf()
-
-        cut_right = random.choice([True, False])
-
-        if cut_right and current.right_slit is None:
-            return
-        if not cut_right and current.left_slit is None:
-            return
-
-        while True:
-            if cut_right:
-                if current.right_slit is None:
-                    return
-
-                if current.right_slit.is_leaf():
-                    current.right_slit = None
-                    return
-            else:
-                if current.left_slit is None:
-                    return
-
-                if current.left_slit.is_leaf():
-                    current.left_slit = None
-                    return
-
-            if random.random() < 0.5:
-                current = current.left_slit
-            else:
-                current = current.right_slit
 
 
 # Objectives:
@@ -141,44 +29,44 @@ class SlicingTree:
 # 3. Maximize the average quality of the slits
 # 4. Minimize the complexity of the slits
 class CoilSlitting(ElementwiseProblem):
-    def __init__(self, max_rectangle_size: float, min_rectangle_size: float):
+    def __init__(
+        self,
+        *,
+        sheet_width: int,
+        sheet_height: int,
+        max_rectangle_size: float,
+        min_rectangle_size: float,
+    ):
         self.max_rectangle_size = max_rectangle_size
         self.min_rectangle_size = min_rectangle_size
-        super().__init__(n_var=1, n_obj=4, n_constr=0, type=SlicingTree)
+        self.sheet_width = sheet_width
+        self.sheet_height = sheet_height
+        # TODO: Change n_obj
+        super().__init__(n_var=1, n_obj=2, n_constr=0, type=SlicingTree)
 
     def _evaluate(self, slitting: list[SlicingTree], out, *args, **kwargs):
-        f1 = slitting[0].number_of_slits
-        f2 = slitting[0].average_slit_size()
-        f3 = slitting[0].average_slit_quality()
-        f4 = slitting[0].complexity()
+        f1 = slitting[0].size
+        f2 = average_rectangle_size(slitting[0], self.sheet_width, self.sheet_height)
+        # f3 = slitting[0].average_slit_quality()
+        # f4 = slitting[0].complexity()
 
-        out["F"] = np.column_stack([f1, -f2, -f3, f4])
-
-
-def generate_random_slitting_tree_root(size: int) -> Optional[Slit]:
-    if size == 0:
-        return None
-
-    size -= 1
-
-    left_size = random.randint(0, size)
-    right_size = size - left_size
-
-    return Slit(
-        horizontal=random.choice([True, False]),
-        offset=random.random(),
-        left_slit=generate_random_slitting_tree_root(left_size),
-        right_slit=generate_random_slitting_tree_root(right_size),
-    )
+        # out["F"] = np.column_stack([f1, -f2, -f3, f4])
+        out["F"] = np.column_stack([f1, -f2])
 
 
 class RandomSlicingTreeSampling(Sampling):
     def _do(self, problem, n_samples, **kwargs):
-        X = np.full((n_samples, 1), None, dtype=object)
+        # X = np.full((n_samples, 1), None, dtype=object)
 
-        for individual_id in range(n_samples):
-            # doesnt include minimal rectangle size
-            X[individual_id, 0] = SlicingTree.random(MAX_NUMBER_OF_SLITS)
+        # for individual_id in range(n_samples):
+        #     # doesnt include minimal rectangle size
+        #     X[individual_id, 0] = SlicingTree.random(MAX_NUMBER_OF_SLITS)
+        X = np.array(
+            [
+                (generate_random_slitting_tree(random.randint(2, MAX_NUMBER_OF_SLITS)),)
+                for _ in range(n_samples)
+            ]
+        )
 
         return X
 
@@ -195,12 +83,7 @@ class SwapSubtreeCrossover(Crossover):
             child1 = deepcopy(parent1)
             child2 = deepcopy(parent2)
 
-            node1 = child1.get_random_node()
-            node2 = child2.get_random_node()
-
-            if node1 is not None and node2 is not None:
-                node1.left_slit, node2.left_slit = node2.left_slit, node1.left_slit
-                node1.right_slit, node2.right_slit = node2.right_slit, node1.right_slit
+            swap_random_subtree(child1, child2)
 
             Y[0, k, 0], Y[1, k, 0] = child1, child2
 
@@ -212,23 +95,13 @@ class AddSlittingAndNudgeMutation(Mutation):
         super().__init__()
 
     def _do(self, problem, X, **kwargs):
-        for i in range(X.shape[0]):
-
-            node = X[i, 0].get_random_node()
-            if node is not None:
-                node.offset += np.random.normal(0, 0.2)
-                node.offset = max(0, min(1, node.offset))
-
-            if random.random() < 0.5:
-                X[i, 0].add_random_slit()
-            else:
-                X[i, 0].remove_random_leaf()
-
         return X
 
 
 if __name__ == "__main__":
-    problem = CoilSlitting(max_rectangle_size=-1, min_rectangle_size=-1)
+    problem = CoilSlitting(
+        max_rectangle_size=-1, min_rectangle_size=-1, sheet_width=100, sheet_height=100
+    )
     algorithm = NSGA2(
         pop_size=POPULATION_SIZE,
         eliminate_duplicates=NoDuplicateElimination(),
---
+++ slicing_tree.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+import random
+from typing import Any
+from anytree import NodeMixin
+
+
+@dataclass
+class Slit(NodeMixin):
+    horizontal: bool
+    offset: float  # percentage of the width/height
+
+
+@dataclass
+class Rectangle:
+    width: float
+    height: float
+    sensors: Any
+
+
+def generate_random_slitting_tree(size: int) -> Slit:
+    if size == 1:
+        return Slit(horizontal=random.choice([True, False]), offset=random.random())
+
+    size -= 1
+
+    left_size = random.randint(0, size)
+    right_size = size - left_size
+
+    children = []
+    if left_size > 0:
+        children_left = (generate_random_slitting_tree(left_size),)
+        children += children_left
+
+    if right_size > 0:
+        children_right = (generate_random_slitting_tree(right_size),)
+        children += children_right
+
+    node = Slit(
+        horizontal=random.choice([True, False]),
+        offset=random.random(),
+    )
+
+    node.children = children
+    return node
+
+
+def swap_random_subtree(tree1: Slit, tree2: Slit) -> None:
+    node1 = random.choice(tree1.descendants)
+    node2 = random.choice(tree2.descendants)
+
+    node1.parent, node2.parent = node2.parent, node1.parent
+
+
+def get_rectangles(node: Slit, width: float, height: float) -> list[Rectangle]:
+
+    height1 = height * node.offset if node.horizontal else height
+    height2 = height - height1 if node.horizontal else height
+
+    width1 = width * node.offset if not node.horizontal else width
+    width2 = width - width1 if not node.horizontal else width
+
+    if not node.children:
+        return [
+            Rectangle(width1, height1, None),
+            Rectangle(width2, height2, None),
+        ]
+
+    rectangles_children1 = get_rectangles(node.children[0], width1, height1)
+
+    if len(node.children) == 1:
+        return rectangles_children1
+
+    rectangles_children2 = get_rectangles(node.children[1], width2, height2)
+
+    return rectangles_children1 + rectangles_children2
+
+
+def average_rectangle_size(tree: Slit, sheet_width: int, sheet_height: int) -> float:
+    rectangles = get_rectangles(tree, sheet_width, sheet_height)
+    return sum(rectangle.width * rectangle.height for rectangle in rectangles) / len(
+        rectangles
+    )
